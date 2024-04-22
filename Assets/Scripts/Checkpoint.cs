8a037b03652f1363c2b14345eb71dc3bf2dff79d
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2024-04-22 11:07:11 +0200

    started checkpoints and shadows

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            //other.GetComponent<PlayerHealth>().SetRespawnPos(gameObject.transform);
+            //this.enabled = false;
+        }
+    }
+}
