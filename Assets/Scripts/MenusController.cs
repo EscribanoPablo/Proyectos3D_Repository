d5498a2b39c1b55e84a00e7bde00073ffca8b4a5
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2024-05-02 00:38:42 +0200

    Scripting menus

--- Assets/Scripts/MenusController.cs
+++ Assets/Scripts/MenusController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenusController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartButtonPressed()
     {
-        
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            SceneManager.LoadScene("Alpha_Level");
+        }
+        else
+        {
+            //FindObjectOfType<PauseMenuController>().UnpauseGame();
+        }
+    }
+
+    public void SettingsButtonPressed()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void ExitButtonPressed()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 }
--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class SettingsController : MonoBehaviour
 {
     [SerializeField]
     private TMP_Dropdown qualityDropdown;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    //[SerializeField]
+    //private Slider sfxVolume;
+    //[SerializeField]
+    //private Slider musicVolume;
+    //
+    //[SerializeField]
+    //private AudioMixer audioMixer;
 
     public void ChangeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(qualityDropdown.value);
+    }
+
+    public void ChangeVolume()
+    {
+        //audioMixer.SetFloat("musicVol", musicVolume.value);
+        //audioMixer.SetFloat("sfxVol", sfxVolume.value);
     }
 }
