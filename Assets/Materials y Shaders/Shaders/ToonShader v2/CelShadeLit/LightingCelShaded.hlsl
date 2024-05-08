a3c6b99adba21dca44a6413bafee93cbb59bcdc0
GuillemTio <gtio@edu.tecnocampus.cat>
2024-05-09 00:43:46 +0200

    NUEVO TOON SHADER

--- Assets/Materials y Shaders/Shaders/ToonShader v2/CelShadeLit/LightingCelShaded.hlsl
+++ Assets/Materials y Shaders/Shaders/ToonShader v2/CelShadeLit/LightingCelShaded.hlsl
@@ -12,6 +12,7 @@ struct EdgeConstants {
 
 };
 
+// Datos que necesita ek shader para los diferentes cálculos y funciones
 struct SurfaceVariables {
 
    float smoothness;
@@ -29,18 +30,25 @@ struct SurfaceVariables {
 };
 
 float3 CalculateCelShading(Light l, SurfaceVariables s) {
-   float attenuation = 
+   
+    //Convertimos la sombra a un borde sin degradado para imitar bien el estilo toon,
+    // al igual que se hace después con la difusa, la specular y el rim
+    float attenuation = 
       smoothstep(0.0f, s.ec.distanceAttenuation, l.distanceAttenuation) * 
       smoothstep(0.0f, s.ec.shadowAttenuation, l.shadowAttenuation);
 
+      // calculamos la difusa y la establecemos entre 0 y 1
    float diffuse = saturate(dot(s.normal, l.direction));
    diffuse *= attenuation;
 
+   //con la direccion de la luz y de la vista, hacemos un vector que nos servirá
+   // para saber la specular basándonos en modelo Blinn-Phong
    float3 h = SafeNormalize(l.direction + s.view);
    float specular = saturate(dot(s.normal, h));
    specular = pow(specular, s.shininess);
    specular *= diffuse;
 
+   //añadimos una iluminación extra que aparezca para complementar el brillo de la specular
    float rim = 1 - dot(s.view, s.normal);
    rim *= pow(diffuse, s.rimThreshold);
 
@@ -62,7 +70,8 @@ void LightingCelShaded_float(float Smoothness,
       float3 Position, float3 Normal, float3 View, float EdgeDiffuse,
       float EdgeSpecular, float EdgeDistanceAttenuation,
       float EdgeShadowAttenuation, float EdgeRim, out float3 Color) {
-
+// arriba cogemos las variables que después pide el material
+// y abajo las definimos para utilizarlo en código
 #if defined(SHADERGRAPH_PREVIEW)
    Color = half3(0.5f, 0.5f, 0.5f);
 #else
@@ -80,6 +89,8 @@ void LightingCelShaded_float(float Smoothness,
    s.ec.shadowAttenuation = EdgeShadowAttenuation;
    s.ec.rim = EdgeRim;
 
+
+   // con esto calculamos las coordenadas de las sombras que afectan al objecto desde el URP de Unity
 #if SHADOWS_SCREEN
    float4 clipPos = TransformWorldToHClip(Position);
    float4 shadowCoord = ComputeScreenPos(clipPos);
@@ -87,8 +98,11 @@ void LightingCelShaded_float(float Smoothness,
    float4 shadowCoord = TransformWorldToShadowCoord(Position);
 #endif
 
+// La función MainLight es propia de URP
    Light light = GetMainLight(shadowCoord);
    Color = CalculateCelShading(light, s);
+
+   // Esto hace que tengamos en cuenta tambien las luces externas del entorno.
 
    int pixelLightCount = GetAdditionalLightsCount();
    for (int i = 0; i < pixelLightCount; i++) {
