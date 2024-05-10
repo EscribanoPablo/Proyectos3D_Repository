5255d2db42e4079b46e9aa42fcbe33efbbce8f9c
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2024-05-10 20:56:20 +0200

    punch rebotao (haciendolo)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,20 +9,6 @@ public class PlayerController : MonoBehaviour, IRestartLevelElement
 
     private void Awake()
     {
-        //Si no hay ningun player en la escena, el player va a ser este
-        //if (GameController.GetGameController().GetPlayer() == null)
-        //{
-        //    //GameController.GetGameController().AddRestartGameElement(this);
-        //    GameController.GetGameController().player = this;
-        //    GameController.GetGameController().AddRestartLevelElement(this);
-        //    GameObject.DontDestroyOnLoad(gameObject);
-        //}
-        //else // Y si lo hay, destruyeme el anterior player y te quedas con este player
-        //{
-        //    GameController.GetGameController().player.SetRespawnPos(transform);
-        //    GameObject.Destroy(GameController.GetGameController().GetPlayer().gameObject);
-        //}
-
         GameController.GetGameController().SetCurrentPlayer(this);
         GameController.GetGameController().AddRestartLevelElement(this);
     }
@@ -48,4 +34,5 @@ public class PlayerController : MonoBehaviour, IRestartLevelElement
         transform.position = startPosition;
         transform.rotation = startRotation;
     }
+    
 }
