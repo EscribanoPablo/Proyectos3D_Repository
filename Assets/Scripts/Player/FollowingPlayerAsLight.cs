242db081e67a15bf9492b6e65e26739ae94f5bf3
GuillemTio <gtio@edu.tecnocampus.cat>
2024-05-19 18:11:20 +0200

    light following player EN PREFABS

--- Assets/Scripts/Player/FollowingPlayerAsLight.cs
+++ Assets/Scripts/Player/FollowingPlayerAsLight.cs
@@ -5,35 +5,42 @@ public class FollowingPlayerAsLight : MonoBehaviour
 
     [SerializeField] float heightOffset = 10;
     //[SerializeField] float rightOffset;
-    [SerializeField] float cameraSpeed = 1;
+    [SerializeField] float lightMovementSpeed = 1;
+    [SerializeField] float lightRotateSpeed = 3;
 
     private GameObject player;
-    //private GameObject mainCamera;
+    private GameObject mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        //mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePosition();
         UpdateRotation();
+        UpdatePosition();
     }
 
     private void UpdatePosition()
     {
         Vector3 abovePlayerPosition = new Vector3(player.transform.position.x, player.transform.position.y + heightOffset, player.transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, abovePlayerPosition, Time.deltaTime * cameraSpeed);
+        transform.position = Vector3.Lerp(transform.position, abovePlayerPosition, Time.deltaTime * lightMovementSpeed);
     }
 
     private void UpdateRotation()
     {
-        Vector3 directionToLook = player.transform.position - transform.position;
-        transform.forward = Vector3.Lerp(transform.forward.normalized, directionToLook.normalized, Time.deltaTime * cameraSpeed);
+       //transform.rotation = Quaternion.Euler( transform.rotation.eulerAngles.x , mainCamera.transform.rotation.eulerAngles.y + 90, transform.rotation.eulerAngles.z);
+       //Quaternion lookRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+       //transform.rotation = Quaternion.Euler(lookRotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        Quaternion lookRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lightRotateSpeed);
+        //Vector3 directionToLook = player.transform.position - transform.position;
+        //transform.forward = Vector3.Lerp(transform.forward.normalized, directionToLook.normalized, Time.deltaTime * cameraSpeed);
         //transform.LookAt(player.transform);
     }
 }
