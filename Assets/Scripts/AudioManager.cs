71dfac0fb17ed3f89a18522bde8c82124c427f4e
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2024-05-20 23:55:32 +0200

    adding sounds to the project

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,17 +9,64 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     AudioSource audioSFX;
 
-    [Header("AudioClips")]
-    AudioClip menuMusic;
-    AudioClip backgroundLevelMusic;
+    [Header("AudioClips_Music")]
+    public AudioClip menuMusic;
+    public AudioClip backgroundLevelMusic;
+    
+    [Header("AudioClips_Player")]
+    public List<AudioClip> ambientSounds;
+    public List<AudioClip> JumpSound;
+    public AudioClip DoubleJumpSound;
+    public AudioClip DashSound;
+    public AudioClip WallJumpSound;
+    public List<AudioClip> ShootSound;
+    public AudioClip FallingToGroundSound;
+    public List<AudioClip> RecieveDamageSound;
+    public AudioClip DieSound;
+    public AudioClip RespawnSound;
 
-    public void playMusic(AudioClip musicClip)
+    [Header("AudioClips_Environment/Obstacles")]
+    public List<AudioClip> GrabCollectibleSound;
+    public AudioClip ActivateCheckPointSound;
+    public AudioClip rotatorySpikesSound;
+    public AudioClip groundSpikesSound;
+    public AudioClip flipPlatformSound;
+    public AudioClip fallPlatformSound;
+    public AudioClip breakingBoxSound;
+    public AudioClip breakingGlassSound;
+    public AudioClip fireCircleSound;
+    public AudioClip punchTrapSound;
+    public AudioClip buttonSpinSound;
+    public AudioClip doorOpenSound;
+    public AudioClip bombMovingSound;
+    public AudioClip bombAttackDeathSound;
+    public AudioClip levelEndedSound;
+    public AudioClip lightTurningOnSound;
+
+    [Header("AudioClips_UI")]
+    public AudioClip transitionsSound;
+    public AudioClip movingInButtonsSound;
+    public AudioClip pressingButtonSounds;
+
+
+    public void SetPlaySfx(AudioClip sfxClip)
     {
-        audioMusic.clip = musicClip;
+        playSFX(sfxClip);
+    }
+
+    private void Start()
+    {
+        audioMusic.clip = backgroundLevelMusic;
         audioMusic.Play();
     }
 
-    public void playSFX(AudioClip sfxClip)
+    //public void playMusic(AudioClip musicClip)
+    //{
+    //    audioMusic.clip = musicClip;
+    //    audioMusic.Play();
+    //}
+
+    private void playSFX(AudioClip sfxClip)
     {
         audioSFX.PlayOneShot(sfxClip);
     }
