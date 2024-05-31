23031f48a4b8ccf1b85592d0b9c17bbe9a30472a
GuillemTio <gtio@edu.tecnocampus.cat>
2024-05-31 19:55:38 +0200

    transiciones pt 1

--- Assets/Scripts/PlayTransition.cs
+++ Assets/Scripts/PlayTransition.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 public class PlayTransition : MonoBehaviour
 {
+    Animation animationManager;
+    [SerializeField] AnimationClip blackAnim;
+    [SerializeField] AnimationClip transparentAnim;
+    [SerializeField] AnimationClip inSceneTransitionAnim;
+    [SerializeField] AnimationClip outSceneTransitionAnim;
+
+    private void Start()
+    {
+        animationManager = GetComponent<Animation>();
+    }
+
+
     private void OnEnable()
     {
-        gameObject.GetComponent<Animator>().SetBool("ApplyTransition", true);
+        //gameObject.GetComponent<Animator>().SetBool("ApplyTransition", true);
+    }
+
+    private void SetBlack()
+    {
+        animationManager.Play(blackAnim.name);
+    }
+
+    private void SetTransparent()
+    {
+        animationManager.Play(transparentAnim.name);
+    }
+
+    private void EnterSceneAnimation()
+    {
+        animationManager.Play(inSceneTransitionAnim.name);
+    }
+
+    private void QuitSceneAnimation()
+    {
+        animationManager.Play(outSceneTransitionAnim.name);
     }
 }
